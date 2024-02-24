8324b855392eefe0ae8aa6e100c4b14df0c62585
Faisal Khan <faisalfazily@gmail.com>
2024-02-24 21:44:46 +0100

    update log

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -61,14 +61,14 @@ const Sidebar = () => {
     },
   ];
   return (
-    <div className="space-y-4 p-y-4 flex flex-col h-full bg-[#111827] text-white">
+    <div className="space-y-4 p-y-4 flex flex-col h-full bg-[#111827] text-white mt-2">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative h-8 w-8 mr-4">
-            <Image fill alt="Logo" src="/logo.png" />
+            <Image fill alt="Logo" src="/logo.svg" />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
-            Genius
+            Intellecta
           </h1>
         </Link>
 
