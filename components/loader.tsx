88ce6159b2db37462de015efc4e2fe5b2477fc2f
Faisal Khan <faisalfazily@gmail.com>
2024-02-26 10:48:11 +0100

    Video function

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
-
-const Loader = () => {
+interface Props {
+  message?: string;
+  waitingMessage?: boolean;
+}
+const Loader = ({ message, waitingMessage }: Props) => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
       <div className="w-10 h-10 relative animate-spin">
         <Image alt="logo" src="/logo.svg" fill />
       </div>
       <p className="text-sm text-muted-foreground">
-        Intellecta is thinking ...
+        {message || "Intellecta is thinking ..."}
+        {waitingMessage && (
+          <p className="text-[10px]">
+            Sometimes it can take up to 20 minutes due to rate limitations and
+            cold start.
+          </p>
+        )}
       </p>
     </div>
   );
